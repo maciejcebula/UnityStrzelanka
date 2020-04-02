771072599b4e4c1afebf3d4289fa632c4824927b
Maciej <maciejcebula45@gmail.com>
2020-04-02 18:11:25 +0200

    Gotowe
    Gotowe cwiczenie 3d

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float xRot = 0f;
+    public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+
+    }
+}
+
